cf378482efac71962e1e61fc4a839c3b6e05a550
Samuel Utbult <samuel.utbult@hotmail.com>
2016-07-31 13:49:05 +0200

    Implementation of Store and a simple counter test

---
+++ include/redux.hpp
@@ -0,0 +1,74 @@
+/*
+Copyright (c) 2016 Samuel Utbult
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef REDUX_H
+#define REDUX_H
+
+#include <vector>
+
+namespace redux {
+
+template <typename STATE_T, typename ACTION_T>
+class Store {
+	public:
+		typedef STATE_T (*Reducer) (STATE_T, ACTION_T);
+		typedef void (*Subscriber) (STATE_T);
+
+		Store(Reducer, STATE_T);
+		void subscribe(Subscriber);
+		void dispatch(ACTION_T);
+		STATE_T getState();
+
+	private:
+		Reducer reducer;
+		STATE_T state;
+		std::vector<Subscriber> subscribers;
+};
+
+template <typename STATE_T, typename ACTION_T>
+Store<STATE_T, ACTION_T>::Store(Reducer reducer, STATE_T initialState) :
+	reducer(reducer),
+	state(initialState) {
+}
+
+template <typename STATE_T, typename ACTION_T>
+void Store<STATE_T, ACTION_T>::subscribe(Subscriber subscriber) {
+	subscribers.push_back(subscriber);
+}
+
+template <typename STATE_T, typename ACTION_T>
+void Store<STATE_T, ACTION_T>::dispatch(ACTION_T action) {
+	state = reducer(state, action);
+
+	for(int i = 0; i < subscribers.size(); i++) {
+		subscribers[i](state);
+	}
+}
+
+template <typename STATE_T, typename ACTION_T>
+STATE_T Store<STATE_T, ACTION_T>::getState() {
+	return state;
+}
+
+};
+
+#endif
---
+++ tests/tests.cpp
@@ -0,0 +1,87 @@
+/*
+Copyright (c) 2016 Samuel Utbult
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "redux.hpp"
+#include "catch.hpp"
+#include <iostream>
+
+using namespace redux;
+
+enum CounterAction {
+	INCREMENT,
+	DECREMENT
+};
+
+int counterReducer(int state, CounterAction action) {
+	switch(action) {
+		case INCREMENT:
+			return state + 1;
+
+		case DECREMENT:
+			return state - 1;
+
+		default:
+			return state;
+	}
+}
+
+int counterTemp[2];
+void counterSubscriber(int n) {
+	counterTemp[0] = n;
+}
+void secondCounterSubscriber(int n) {
+	counterTemp[1] = n;
+}
+
+TEST_CASE("A simple counter", "[redux]") {
+	Store<int, CounterAction> store(counterReducer, 0);
+
+	SECTION("The counter increments once") {
+		store.dispatch(INCREMENT);
+		REQUIRE(store.getState() == 1);
+
+		SECTION("The counter increments a second time") {
+			store.dispatch(INCREMENT);
+			REQUIRE(store.getState() == 2);
+		}
+		SECTION("The counter decrements") {
+			store.dispatch(DECREMENT);
+			REQUIRE(store.getState() == 0);
+		}
+	}
+	SECTION("The counter decrements once") {
+		store.dispatch(DECREMENT);
+		REQUIRE(store.getState() == -1);
+	}
+	SECTION("The store can be subscribed to") {
+		store.subscribe(counterSubscriber);
+		store.dispatch(INCREMENT);
+		REQUIRE(counterTemp[0] == 1);
+
+		SECTION("With two subscribers") {
+			store.subscribe(secondCounterSubscriber);
+			store.dispatch(INCREMENT);
+			REQUIRE(counterTemp[0] == 2);
+			REQUIRE(counterTemp[1] == 2);
+		}
+	}
+}
